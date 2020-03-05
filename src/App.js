d6444d71682a7bd964ba1214ab5f44b1c94d3e17
Arek Chatmajian <arek.chatmajian@gmail.com>
2020-03-04 23:38:38 -0500

    Added shuffle and state logic

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
-import React from 'react';
-import Hero from "./components/Hero.js";
-import Grid from "./components/Grid.js";
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Score from "./components/Score";
 import images from "./images.json";
-import './styles/App.css';
+import "./App.css";
 
-// images.map, then generate picture into a div
+class App extends Component {
+  // Setting state
+  state = {
+    images,
+    clickedId: [],
+    score: 0,
+    goal: 8,
+    status: ""
+  };
 
-function App() {
-  const shuffle = () => {
-    // Shuffle logic goes here
-  };
-  const imageClick = () => {
-    // Image click logic goes here
-  };
-  const gameOver = () => {
-    // Game over logic goes here
-  };
-  return (
-    <div className="App">
-      <Hero />
-      <Grid pickle={images}/>
+  // Shuffle logic when image is clicked
+  shuffleScoreCard = id => {
+    let clickedId = this.state.clickedId;
+
+    if(clickedId.includes(id)){
+      this.setState({ clickedId: [], score: 0, status: "You Lose" });
+      return;
+    }else{
+      clickedId.push(id)
+
+      if(clickedId.length === 8){
+        this.setState({score: 8, status: "You Win", clickedId: []});
+        console.log('You Win');
+        return;
+      }
+
+      this.setState({ images, clickedId, score: clickedId.length, status: " " });
+
+      for (let i = images.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [images[i], images[j]] = [images[j], images[i]];
+      }
+    }
+  }
+
+  // Renders cards using .map from json objects array
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Pied Piper Memory Game</h1>
+          <p className="App-intro">
+          Click on an image to earn points, but don't click on any more than once!
+          </p>
+          <br></br>
+          <Score total={this.state.score}
+               goal={8}
+               status={this.state.status}
+               />
+        </header>
+
+        <Wrapper>
+          {this.state.images.map(images => (
+            <Card
+              shuffleScoreCard={this.shuffleScoreCard}
+              id={images.id}
+              name={images.name}
+              image={images.url}
+            />
+          ))}
+        </Wrapper>
     </div>
-
-  );
+    );
+  }
 }
 
 export default App;
